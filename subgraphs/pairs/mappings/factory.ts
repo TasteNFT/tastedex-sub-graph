cf00d81a750934ae84181375e0d550df4c510219
Vadim <xboxfadeev@gmail.com>
2022-02-14 15:58:21 +0700

    feat: Fix address issue

--- subgraphs/pairs/mappings/factory.ts
+++ subgraphs/pairs/mappings/factory.ts
@@ -5,7 +5,7 @@ import { PairCreated } from "../generated/Factory/Factory";
 import { fetchDecimals, fetchName, fetchSymbol } from "./utils/erc20";
 
 // Constants
-let FACTORY_ADDRESS = "0xca143ce32fe78f1f7019d7d551a6402fc5350c73";
+let FACTORY_ADDRESS = "0x789A39AE5eE2dc6b1B8886b7AaF0e399e1E7Cad4";
 
 // BigNumber-like references
 let ZERO_BI = BigInt.fromI32(0);
